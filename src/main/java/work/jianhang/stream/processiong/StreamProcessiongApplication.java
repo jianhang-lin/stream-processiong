dc307d853f455c1f68538a2f43c50bf2ad937e66
Grant Lin <grant.lin@42-q.com>
2020-01-16 11:32:01 +0800

    Initial commit

---
+++ src/main/java/work/jianhang/stream/processiong/StreamProcessiongApplication.java
@@ -0,0 +1,13 @@
+package work.jianhang.stream.processiong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamProcessiongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamProcessiongApplication.class, args);
+    }
+
+}
